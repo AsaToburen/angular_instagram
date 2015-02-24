836ec8f91d5356cb9dc11798a53ff3e6c393b0d9
Asa Toburen <atopettit@gmail.com>
2015-02-24 07:11:03 -0700

    added fa icon and improved directives

--- js/app.js
+++ js/app.js
@@ -1,65 +1,52 @@
-
 angular.module('InstaMod', ['ngAnimate'])
-  .controller('searchCtrl', ['$scope', '$timeout', '$q', '$http', function($scope, $timeout, $q, $http) {
-    function wait() {
-      return $q(function(resolve, reject) {
-        $timeout(function() {
-          resolve();
-        }, 2000);
-      });
-    }
+  .controller('searchCtrl', ['$scope', '$timeout', '$q', '$http',
+    function($scope, $timeout, $q, $http) {
 
-    function emptyImageData() {
-      $scope.imageData = {
-        imageUrl : [],
-        instagramUrl : []
-      };
-      return $scope.imageData;
-    }
-
-    $scope.loadMsg = false;
-    $scope.successMsg = false;
-    $scope.errorMsg = false;
-    $scope.submitText = true;
-
-    function parseCallback(result) {
-      return wait().then(function() {
-        for(var i=0; i<20; i++) {
-          $scope.imageData.imageUrl.push(result.data[i].images.low_resolution.url);
-          $scope.imageData.instagramUrl.push(result.data[i].link);
+      function emptyImageData() {
+        $scope.imageData = {
+          imageUrl: [],
+          instagramUrl: []
+        };
+        return $scope.imageData;
       }
-      console.log($scope.imageData);
+
       $scope.loadMsg = false;
-      $scope.successMsg = true;
-      $scope.submitText = true;
-    });
-  }
+      $scope.errorMsg = false;
 
-    $scope.searchInstagram = function(userInput) {
-      emptyImageData();
-      $scope.data.queryValue = userInput || null;
-      $scope.data.search_text = null;
-      $scope.loadMsg = true;
-      var url = "https://api.instagram.com/v1/tags/"+ userInput +"/media/recent";
-      var request = {
-        callback: "JSON_CALLBACK",
-        client_id: "1901232dbce1407c82f4c0b4a90152b7"
+      function parseCallback(result) {
+        $scope.loadMsg = false;
+        for (var i = 0; i < 20; i++) {
+          $scope.imageData.imageUrl.push(result.data[i].images.low_resolution
+            .url);
+          $scope.imageData.instagramUrl.push(result.data[i].link);
+        }
+      }
+
+      $scope.searchInstagram = function(userInput) {
+        emptyImageData();
+        $scope.data.queryValue = userInput || null;
+        $scope.data.search_text = null;
+        $scope.loadMsg = true;
+        var url = "https://api.instagram.com/v1/tags/" + userInput +
+          "/media/recent";
+        var request = {
+          callback: "JSON_CALLBACK",
+          client_id: "1901232dbce1407c82f4c0b4a90152b7"
+        };
+
+        $http({
+            method: 'JSONP',
+            url: url,
+            params: request
+        })
+          .success(function(result) {
+            parseCallback(result);
+          })
+          .error(function() {
+            $scope.loadMsg = false;
+            $scope.successMsg = false;
+            $scope.errorMsg = true;
+          });
       };
-    
-    $http({
-      method: 'JSONP',
-      url: url,
-      params: request
-    })
-    .success(function( result ) {
-      parseCallback(result);
-    })
-      .error(function(){
-       $scope.loadMsg = false;
-       $scope.successMsg = false;
-       $scope.errorMsg = true;
-    });
-  };
-
-}]);
-  
+    }
+  ]);
